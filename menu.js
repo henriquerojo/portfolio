0e0e0c18189c2036cfd18dd308ce8b76786d23a5
henriquerojo <henriquerojo10@gmail.com>
2023-06-16 18:39:37 -0300

    atualizando detalhes do menu e fonte

--- menu.js
+++ menu.js
@@ -27,3 +27,8 @@ window.addEventListener("scroll", function () {
     let header = document.querySelector('#header')
     header.classList.toggle('rolagem', window.scrollY > 0)
 })
+
+window.addEventListener("scroll", function () {
+    let header = document.querySelector('#hamburguer')
+    header.classList.toggle('rolagem', window.scrollY > 0)
+})
